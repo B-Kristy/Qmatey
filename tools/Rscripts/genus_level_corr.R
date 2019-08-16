6c9f6a643362bc071a6795e09121ceca26b3d119
brandon <bkristy@vols.utk.edu>
2019-08-16 14:09:23 -0400

    Added correlogram scripts

---
+++ tools/Rscripts/genus_level_corr.R
@@ -0,0 +1,45 @@
+# Load libraries
+library(plyr); library(dplyr)
+library(car)
+library(MASS)
+library(data.table)
+library(ggplot2)
+library(ggcorrplot)
+library(reshape)
+library(stringr)
+
+args <- commandArgs(TRUE)
+
+genus <- read.table(args[1], header = T, sep="\t", check.names=FALSE, fill=TRUE)
+agg = aggregate(. ~ genus, data = genus, mean)
+
+#setwd ("D:/Vbox_Shared/DC_gbs/metagenomes")
+for (i in c(args[2])) {
+  metag <- subset(agg, select=-c(tax_id,species,family,order,class,phylum,kingdom,superkingdom))
+  metag <- subset(metag, select=c(ncol(metag),1:(ncol(metag)-1)))
+  metag <- subset(metag, select=-c(taxname))
+  metag$count <- rowSums(metag[,2:ncol(metag)] > "0")
+  metag$percent <- ((metag$count)/(ncol(metag)-2))*100
+  metag <- subset(metag, percent >= i )
+  metag <- subset(metag, select=-c(count,percent))
+  metag <- setNames(data.frame(t(metag[,-1])), metag[,1])
+  metag_corr <- cor(metag, method = c("spearman")); axis_density <- 2000/nrow(metag_corr)
+  metag_corr <- melt(metag_corr)
+  metag_pmat <- cor_pmat(metag, method = c("spearman"), exact=FALSE)
+  metag_pmat <- melt(metag_pmat)
+  corr <- merge(metag_corr, metag_pmat, by=c("X1","X2"))
+  colnames(corr) <- c("Var1","Var2","coeff","pvalue")
+  corr <- subset(corr, pvalue <= 0.05)
+  
+  
+  plot <- ggplot(corr, aes(x=Var1, y=Var2, fill=coeff, size= pvalue)) +
+    geom_point(aes(size=-pvalue), shape=21) + scale_fill_gradient2(low="red", mid="white", high="cornflowerblue") +
+    theme_bw() + coord_equal() + scale_size(guide = 'none') +
+    labs(x="",y="",fill="Correlation\nCoeficient",size="p-value") +
+    theme(axis.text.x=element_text(size=15, angle=45, vjust=1, hjust=1, margin=margin(0,0,0,0)),
+          axis.text.y=element_text(size=15, margin=margin(0,0,0,0)), panel.grid.major=element_line(colour = "grey95"),
+          legend.title=element_text(size=15), legend.text=element_text(size=10),legend.key.size = unit(0.3, "in"),
+          plot.title = element_text(size=12)) +
+    labs(title= "Genus-Level Correlogram")
+  ggsave(filename=paste("Metagenome",i,"_genus_corr_spearman.tiff",sep=""), plot=plot, width=10, height=10, dpi=600, compression = "lzw", limitsize = FALSE)
+}
---
+++ tools/Rscripts/species_level_corr.R
@@ -0,0 +1,44 @@
+# Load libraries
+library(plyr); library(dplyr)
+library(car)
+library(MASS)
+library(data.table)
+library(ggplot2)
+library(ggcorrplot)
+library(reshape)
+library(stringr)
+
+args <- commandArgs(TRUE)
+
+species <- read.table(args[1], header = T, sep="\t", check.names=FALSE, fill=TRUE)
+agg = aggregate(. ~ species, data = species, mean)
+
+for (i in c(args[2])) {
+  metag <- subset(agg, select=-c(tax_id,genus,family,order,class,phylum,kingdom,superkingdom))
+  metag <- subset(metag, select=c(ncol(metag),1:(ncol(metag)-1)))
+  metag <- subset(metag, select=-c(taxname))
+  metag$count <- rowSums(metag[,2:ncol(metag)] > "0")
+  metag$percent <- ((metag$count)/(ncol(metag)-2))*100
+  metag <- subset(metag, percent >= i )
+  metag <- subset(metag, select=-c(count,percent))
+  metag <- setNames(data.frame(t(metag[,-1])), metag[,1])
+  metag_corr <- cor(metag, method = c("spearman")); axis_density <- 2000/nrow(metag_corr)
+  metag_corr <- melt(metag_corr)
+  metag_pmat <- cor_pmat(metag, method = c("spearman"), exact=FALSE)
+  metag_pmat <- melt(metag_pmat)
+  corr <- merge(metag_corr, metag_pmat, by=c("X1","X2"))
+  colnames(corr) <- c("Var1","Var2","coeff","pvalue")
+  corr <- subset(corr, pvalue <= 0.05)
+  
+  
+  plot <- ggplot(corr, aes(x=Var1, y=Var2, fill=coeff, size= ppvalue)) +
+    geom_point(aes(size=-pvalue), shape=21) + scale_fill_gradient2(low="red", mid="white", high="cornflowerblue") +
+    theme_bw() + coord_equal() + scale_size(guide = 'none') +
+    labs(x="",y="",fill="Correlation\nCoeficient",size="p-value") +
+    theme(axis.text.x=element_text(size=10.5, angle=45, vjust=1, hjust=1, margin=margin(0,0,0,0)),
+          axis.text.y=element_text(size=10.5, margin=margin(0,0,0,0)), panel.grid.major=element_line(colour = "grey95"),
+          legend.title=element_text(size=8), legend.text=element_text(size=8),legend.key.size = unit(0.3, "in"),
+          plot.title = element_text(size=7.5)) +
+    labs(title= "Species-Level Correlogram")
+  ggsave(filename=paste("Metagenome",i,"_species_corr_spearman.tiff",sep=""), plot=plot, width=6, height=6, dpi=600, compression = "lzw", limitsize = FALSE)
+}
